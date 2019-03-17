f90242e26e928d5fbb4ff5de53922b74bff641dc
Nabeel Ahmed <nab2320@gmail.com>
2019-03-16 18:33:00 -0700

    cash register

--- CIS_22B_FinalProject/main.cpp
+++ CIS_22B_FinalProject/main.cpp
@@ -9,53 +9,60 @@ void DisplayMenu();
 void CashierDisplayMenu();
 int binarySearch(Book array[], int size, string value);
 void DisplayTotalMenu(double Total);
+void selectionSort(Book array[], int size);
 
 
 
 int main() {
-
-
-
+	
 	Book Becoming("9781524763138", "Becoming", "Michelle Obama", "Crown Publishing Group", 5, 15.00, 19.50);
 	Book WhereTheCrawdadsSing("9780735219090", "Where the Crawdads Sing", "Delia Owens", "Penguin Publishing Group", 5, 10.00, 15.60);
 	Book YouAreMyHappy("9780062931030", "You Are My Happy", "Hoda Kotb, Suzie Mason(Illustrator)", "HarperCollins Publishers", 5, 10.56, 17.09);
 	Book TheUmbrellaAcademy("9781593079789", "The Umbrella Academy, Volume 1: Apocalypse Suite", "Gerard Way, Gabriel Ba (Artist)", "Dark Horse Comics", 5, 9.50, 11.90);
 	Book TheMeullerReport("9781510750166", "The Meuller Report", "Robert S. Mueller III", "Skyhorse", 5, 10.56, 17.09);
 
-
-
 	Book arr[5] = { Becoming, WhereTheCrawdadsSing, YouAreMyHappy, TheUmbrellaAcademy, TheMeullerReport };
-
 	int selection;
-
-
+	selectionSort(arr, 5);
 
 	DisplayMenu();
 	cin >> selection;
+	
 	
 	if (selection == 1)
 	{
 		
 		string isbn;
 		double total = 0.0;
+		int position;
+		string receipt[100];
+		int count = 0;
 
 		do
 		{
 			
+
 			
-			int position;
 
 			CashierDisplayMenu();
 			cin >> isbn;
 			if (isbn != "Q")
 			{
+				
+				cout << isbn << endl;
 				position = binarySearch(arr, 5, isbn);
+				cout << position << endl;
 				cout << arr[position].getRetailPrice() << endl;
 				total += arr[position].getRetailPrice();
-				
+				receipt[count] = arr[position].getTitle();
+				count++;
+
+
 			}
 
 		} while (isbn != "Q");
+		cout << receipt << endl;
+
 		DisplayTotalMenu(total);
 		
 	}
@@ -128,9 +135,34 @@ int binarySearch(Book array[], int size, string value) {
 			position = middle;
 		}
 		else if (array[middle].getISBN() > value)  // If value is in lower half
+		{
 			last = middle - 1;
+		}
 		else
+		{
 			first = middle + 1;           // If value is in upper half
+		}
 	}
 	return position;
+}
+
+void selectionSort(Book array[], int size)
+{
+	int startScan, minIndex;
+	string minValue;
+	for (startScan = 0; startScan < (size - 1); startScan++)
+	{
+		minIndex = startScan;
+		minValue = array[startScan].getISBN();
+		for (int index = startScan + 1; index < size; index++)
+		{
+			if (array[index].getISBN() < minValue)
+			{
+				minValue = array[index].getISBN();
+				minIndex = index;
+			}
+		}
+		array[minIndex] = array[startScan];
+		array[startScan].setISBN(minValue);
+	}
 }
